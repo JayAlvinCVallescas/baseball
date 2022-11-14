2a130777a8fe1006a64b2d4c887592643c9e187f
JayAlvinCVallescas <202080209@psu.palawan.edu.ph>
2022-11-14 10:41:54 +0800

    commit changes

--- projectsite/baseball_/models.py
+++ projectsite/baseball_/models.py
@@ -1,10 +1,43 @@
 from django.db import models
 
 # Create your models here.
-firstname = models.Charfield(max_length=100)
-email = models.EmailField(null=True, blank=True, max_length=100)
-height = models.DecimalField(max_digits=10,decimal_places=5,null=True)
-latitude = models.DecimalField(max_digits=22, decimal_places=16, mull=True, blank=True)
-isActive = models.BooleanField(default=False, verbose_name="Zoning Fee")
-birthdate = models.DateField(default=timezone.now, blank=True)
-coach = models.ForeignKey(Person, on_delete=models.CASCADE)
+# firstname = models.CharField(max_length=100)
+# email = models.EmailField(null=True, blank=True, max_length=100)
+# height = models.DecimalField(max_digits=10,decimal_places=5,null=True)
+# latitude = models.DecimalField(max_digits=22, decimal_places=16, mull=True, blank=True)
+# isActive = models.BooleanField(default=False, verbose_name="Zoning Fee")
+# birthdate = models.DateField(default=timezone.now, blank=True)
+# coach = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+class BaseModel(models.Model):
+        created_at = models.DateTimeField(
+            auto_now_add=True, db_index=True)
+        updated_at = models.DateTimeField(auto_now=True)
+
+        class Meta:
+            abstract = True
+
+class Position(BaseModel):
+    description = models.CharField(max_length=100)
+
+class Person(BaseModel):
+    lastname = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    height = models.DecimalField(max_digits=10, decimal_places=5, null=True)
+    width = models.DecimalField(max_digits=10, decimal_places=5, null=True)
+
+
+class Club(BaseModel):
+    name = models.CharField(max_length=100)
+    coach = models.ForeignKey(Person, on_delete=models.CASCADE)
+    dorm_latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
+    dorm_longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
+
+
+class Play(BaseModel):
+    player = models.ForeignKey(Person, on_delete=models.CASCADE)
+    team = models.ForeignKey(Club, on_delete=models.CASCADE)
+    string_no = models.CharField(max_length=100)
+    pos = models.ForeignKey(Position, on_delete=models.CASCADE)
+    isActive = models.BooleanField(default=False, verbose_name="Zoning Fee")
+
--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ppx1n47k$mtac7*tu2bmly+^3a*ggk2r)^@61(t&)ou*c&-7+@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landingpage',
+    'baseball_',
 ]
 
 MIDDLEWARE = [
